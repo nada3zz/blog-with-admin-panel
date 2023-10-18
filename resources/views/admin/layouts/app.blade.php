61712c5e009c973fc40d0afcbb432f7ec62a507f
nadaeezzdin <nadaeezzdin@gmail.com>
2023-10-19 00:56:02 +0300

    admin setup

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    @include('head');
+
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+        @include('header');
+        
+        @include('footer');
+    </div>
+
+</body>
+</html>
